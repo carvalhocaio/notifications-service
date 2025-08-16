fc3b89d3084e24b4c3f5537619c621cdb9d934b3
Caio Carvalho <caiocarvalho.py@gmail.com>
2025-08-15 22:50:52 -0400

    Refactor to replace `MailService` with `PrismaService` for database-backed notification handling.

--- src/mail/postmark-mail.service.ts
+++
@@ -1,9 +0,0 @@
-import { MailService } from './mail.service';
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class PostmarkMailService implements MailService {
-  sendEmail(): string {
-    return 'Postmark Mail!';
-  }
-}
--- src/mail/smtp-mail.service.ts
+++
@@ -1,9 +0,0 @@
-import { MailService } from './mail.service';
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class SmtpMailService implements MailService {
-  sendEmail(): string {
-    return 'SMTP Mail!';
-  }
-}
